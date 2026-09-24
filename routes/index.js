d3b752df193bd7216edf412a45db5529c1f4a05a
LexKahn <stingwray.mail@gmail.com>
2026-09-24 18:42:06 +0200

    redesign p2

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,34 @@
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var router = express.Router();
+
+var images = {}
+
+fs.readdir('public/images/full', (err, folders) => {
+  folders.forEach(folder => {
+    images[folder] = [];
+    fs.readdir(`public/images/full/${folder}`, (err, files) => {
+      files.forEach(file => {
+        images[folder].push({
+          category: folder,
+          file: `${folder}/${file}`,
+          text: ''
+        });
+      });
+    });
+  });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/nu-avalon', function(req, res, next) {
-  res.render('nuavalon', { title: 'Nu Avalon Dashboard' });
+/* GET image data. */
+router.get('/getImages', function(req, res, next) {
+  console.log(images)
+  res.status(200).json({data: images});
 });
 
 module.exports = router;
